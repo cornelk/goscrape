3a8e07806695b2abaee49df295704a11592c9d92
cornelk <github@codejuggle.dj>
2017-03-07 17:38:55 +0700

    Relinking for elements from a sub dir fixed

--- scraper/html.go
+++ scraper/html.go
@@ -11,31 +11,39 @@ import (
 )
 
 func (s *Scraper) fixFileReferences(URL *url.URL, buf io.Reader) (string, error) {
+	var relativeToRoot string
+	splits := strings.Split(URL.Path, "/")
+	for i := range splits {
+		if len(splits[i]) > 0 {
+			relativeToRoot += "../"
+		}
+	}
+
 	g, err := goquery.NewDocumentFromReader(buf)
 	if err != nil {
 		return "", err
 	}
 
 	g.Find("a").Each(func(_ int, selection *goquery.Selection) {
-		s.fixQuerySelection(URL, "href", selection, true)
+		s.fixQuerySelection(URL, "href", selection, true, relativeToRoot)
 	})
 
 	g.Find("link").Each(func(_ int, selection *goquery.Selection) {
-		s.fixQuerySelection(URL, "href", selection, false)
+		s.fixQuerySelection(URL, "href", selection, false, relativeToRoot)
 	})
 
 	g.Find("img").Each(func(_ int, selection *goquery.Selection) {
-		s.fixQuerySelection(URL, "src", selection, false)
+		s.fixQuerySelection(URL, "src", selection, false, relativeToRoot)
 	})
 
 	g.Find("script").Each(func(_ int, selection *goquery.Selection) {
-		s.fixQuerySelection(URL, "src", selection, false)
+		s.fixQuerySelection(URL, "src", selection, false, relativeToRoot)
 	})
 
 	return g.Html()
 }
 
-func (s *Scraper) fixQuerySelection(URL *url.URL, attribute string, selection *goquery.Selection, page bool) {
+func (s *Scraper) fixQuerySelection(URL *url.URL, attribute string, selection *goquery.Selection, linkIsAPage bool, relativeToRoot string) {
 	src, ok := selection.Attr(attribute)
 	if !ok {
 		return
@@ -60,9 +68,15 @@ func (s *Scraper) fixQuerySelection(URL *url.URL, attribute string, selection *g
 
 	if refStr == "" {
 		refStr = "/" // website root
+	} else {
+		if refStr[0] == '/' && len(relativeToRoot) > 0 {
+			refStr = relativeToRoot + refStr[1:]
+		} else {
+			refStr = relativeToRoot + refStr
+		}
 	}
 
-	if page && refStr[len(refStr)-1] == '/' {
+	if linkIsAPage && refStr[len(refStr)-1] == '/' {
 		refStr += "index.html"
 	}
 	refStr = strings.TrimPrefix(refStr, "/")
