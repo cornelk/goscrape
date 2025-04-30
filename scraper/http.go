1fe4bf98fb36b6e8066db98d3c13e544525a024f
Brian Goad <bdgoad@gmail.com>
2025-04-30 01:31:31 -0400

    Fixes #56 by adding handling for 429 responses (#57)

--- scraper/http.go
+++ scraper/http.go
@@ -3,19 +3,22 @@ package scraper
 import (
 	"bytes"
 	"context"
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
 	"net/url"
+	"strconv"
 	"strings"
+	"time"
 
 	"github.com/cornelk/gotokit/log"
 )
 
-func (s *Scraper) downloadURL(ctx context.Context, u *url.URL) ([]byte, *url.URL, error) {
+func (s *Scraper) downloadURL(ctx context.Context, u *url.URL) (*http.Response, error) {
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, u.String(), nil)
 	if err != nil {
-		return nil, nil, fmt.Errorf("creating HTTP request: %w", err)
+		return nil, err
 	}
 
 	req.Header.Set("User-Agent", s.config.UserAgent)
@@ -30,10 +33,44 @@ func (s *Scraper) downloadURL(ctx context.Context, u *url.URL) ([]byte, *url.URL
 	}
 
 	resp, err := s.client.Do(req)
+
+	return resp, nil
+}
+
+func (s *Scraper) downloadURLWithRetriesFor429(ctx context.Context, u *url.URL) (*http.Response, error) {
+	maxRetries := 10
+
+	for i := 0; i < maxRetries; i++ {
+		resp, err := s.downloadURL(ctx, u)
+		if resp != nil {
+			if resp.StatusCode == http.StatusTooManyRequests {
+				s.logger.Warn(fmt.Sprintf("Too Many Requests. Retrying again (%s/%s)", strconv.Itoa(i+1), strconv.Itoa(maxRetries)), log.String("url", u.String()))
+				// Wait a bit and try again
+				time.Sleep(time.Duration((i+1)*1500) * time.Millisecond) // max total of 82.5 seconds within 10 retries using exponential backoff on each retry
+				continue
+			} else {
+				return resp, err
+			}
+		} else if err != nil {
+			return nil, err
+		}
+		// Success
+		return resp, nil
+	}
+	// Exhausted retries
+	err := errors.New("Exhausted retries for URL " + u.String())
+	return nil, err
+}
+
+func (s *Scraper) downloadURLWithRetries(ctx context.Context, u *url.URL) ([]byte, *url.URL, error) {
+
+	resp, err := s.downloadURLWithRetriesFor429(ctx, u)
 	if err != nil {
 		return nil, nil, fmt.Errorf("sending HTTP request: %w", err)
 	}
-
+	if resp == nil {
+		return nil, nil, fmt.Errorf("HTTP response is nil for %v", u)
+	}
 	defer func() {
 		if err := resp.Body.Close(); err != nil {
 			s.logger.Error("Closing HTTP Request body failed",
